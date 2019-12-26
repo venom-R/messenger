c8f18f60a16a0f8905469cda96445b5c8d52639c
Roma Teleshyk <teleshyk.r@gmail.com>
2019-12-26 22:22:22 +0200

    add fallback for Avatar if user didn't upload photo

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
+import { randomInteger } from '../../helpers';
 import './Avatar.scss';
+
+const avatarBgColors = [
+  '#2196F3',
+  '#00BCD4',
+  '#f44336',
+  '#E91E63',
+  '#009688',
+  '#607D8B',
+  '#FF9800',
+  '#9C27B0',
+  '#9E9E9E',
+  '#9E9E9E',
+];
 
 const Avatar = ({ src, alt, size = 'sm', round = true }) => {
   const avatarClass = classNames({
@@ -8,11 +22,19 @@ const Avatar = ({ src, alt, size = 'sm', round = true }) => {
     [`Avatar_${size}`]: size === 'sm' || size === 'lg',
     Avatar_round: round,
   });
-  return (
-    <span className={avatarClass}>
-      <img src={src} alt={alt} className="Avatar__img" />
-    </span>
-  );
+  const renderContent = () => {
+    if (src) {
+      return <img src={src} alt={alt} className="Avatar__img" />;
+    }
+    const randomColor = avatarBgColors[randomInteger(0, 9)];
+    return (
+      <span style={{ background: randomColor }} className="Avatar__content">
+        {typeof alt === 'string' && alt.length > 0 ? alt.substring(0, 1) : null}
+      </span>
+    );
+  };
+
+  return <span className={avatarClass}>{renderContent()}</span>;
 };
 
 export default Avatar;
