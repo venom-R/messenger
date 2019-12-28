bad5f62ee0691104a074cfc268856b8fa8474b1a
Roma Teleshyk <teleshyk.r@gmail.com>
2019-12-29 00:07:49 +0200

    create layout of ChatFooter

--- src/components/Chat/ChatFooter.js
+++ src/components/Chat/ChatFooter.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { Button, Input, Popover, Tooltip } from 'antd';
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+
+const { TextArea } = Input;
+
+const emojiCategories = ['search', 'recent', 'people', 'nature', 'foods', 'activity'];
+
+const EmojiPicker = props => {
+  return (
+    <Picker
+      set="apple"
+      include={emojiCategories}
+      autofocus={true}
+      darkMode={false}
+      showPreview={false}
+      showSkinTones={false}
+      color="#0a80ff"
+      onSelect={emoji => console.log(emoji)}
+    />
+  );
+};
 
 const ChatFooter = props => {
   return (
     <footer className="ChatFooter">
-      <h2>ChatFooter</h2>
+      <form>
+        <div className="ChatFooter__row">
+          <Popover content={<EmojiPicker />} trigger="click">
+            <Tooltip placement="top" title="Emoji">
+              <Button className="btn btn_secondary mr-3">
+                <Icon icon={['far', 'smile']} />
+              </Button>
+            </Tooltip>
+          </Popover>
+
+          <TextArea placeholder="Write a message" autoSize />
+
+          <Tooltip placement="top" title="Add files">
+            <Button className="btn btn_secondary mx-3">
+              <Icon icon={['fas', 'paperclip']} />
+            </Button>
+          </Tooltip>
+
+          <Button type="primary" htmlType="submit">
+            <Icon icon={['far', 'paper-plane']} />
+          </Button>
+        </div>
+      </form>
     </footer>
   );
 };
