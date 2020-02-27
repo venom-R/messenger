7ee36f94a17bc33a1687ce623021aabc0cb4b89c
Roma Teleshyk <teleshyk.r@gmail.com>
2020-02-27 21:08:43 +0200

    fix styles of favorites and archived links

--- src/modules/navigation/NavigationItem.js
+++ src/modules/navigation/NavigationItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Badge, Tooltip } from 'antd';
 
 const NavigationItem = ({ link, isVisibleBadge, badgeStatus, tooltipTitle, children }) => {
+  const badgeClassNames = classNames('Navigation__badge-container', {
+    'Navigation__badge-container_hidden': !isVisibleBadge,
+  });
   return (
     <Tooltip placement="right" title={tooltipTitle}>
       <a href={link} className="Navigation__link">
-        <Badge
-          status={badgeStatus}
-          className={`Navigation__badge-container ${isVisibleBadge ? '' : 'hidden'}`}>
+        <Badge status={badgeStatus} className={badgeClassNames}>
           {children}
         </Badge>
       </a>
